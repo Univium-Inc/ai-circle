82a1b44b94ed0553f3d95d657c60ef6990394cb1
PatrickF97 <patrick@univium.com>
2025-04-20 18:07:40 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 import dynamic from 'next/dynamic';
 
 // Use dynamic import with SSR disabled since the component uses browser APIs
-const AIGameShow = dynamic(() => import('@/components/AIGameShow'), { 
+const AIGameShow = dynamic(() => import('../components/AIGameShow'), { 
   ssr: false 
 });
 
